014f28297e85eeec73e9f629e6387515753388b6
Dragon <92691789+PoisonDragon704@users.noreply.github.com>
2022-11-04 19:23:09 -0500

    Create apps.js

---
+++ apps/apps.js
@@ -0,0 +1,7 @@
+var p12 = "88226cdb9151"
+var mobileprovision = "1e8020b005ea"
+var password = "AppleP12.com"
+
+function unc0ver() {
+window.open("https://sign.starfiles.co/?ipa=1bca09a52a24&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
